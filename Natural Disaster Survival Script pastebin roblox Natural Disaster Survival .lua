c98efc31454f7e88f22a208bfa6e51b1ed027a2d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:31:53 +0100

    commit zero

---
+++ Natural Disaster Survival Script pastebin roblox Natural Disaster Survival .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+It has: 1) setwalkspeed, 2) setjumpower, 3) AFK wins, 4) no drop damage, 5)teleports, 6) map selection.
+]]
